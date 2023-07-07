1411a04f221b3d36dacc604acc3bdabaff199266
Luis Martinez <rickymtz1999@gmail.com>
2023-07-06 19:15:48 -0500

    add basic code

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();
+const es6Renderer = require("express-es6-template-engine")
+
+server.get('/heartbeat', (req, res) => {
+    res.json({
+        "message": "Heartbeat"
+    });
+});
+
+server.listen(8080, () => {
+    console.log("The server is running at PORT 8080");
+});
