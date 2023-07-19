5062920ca7f5472f07451ab749c23e00189edcea
Henryesc <122941763+Henryesc@users.noreply.github.com>
2023-07-18 19:10:08 -0500

    Review02 (#39)
    * add navs links mock (#4)
    
    * add global styles and removed vite default code (#7)
    
    * add sectioning and view folder (#9)
    
    * add react router dom dependency and routes
    
    * fix href and route
    
    * add styled-components dependency
    
    * moves directories where they are supposed to be
    
    * add burger icon dependency
    
    * add navbar component
    
    * add navbar component
    
    * add testimonial component code
    
    * add testimonials further details
    
    * 017/hero section (#18)
    
    * remove classname y-wrap
    
    * add position relative to root
    
    * add white logo
    
    * add transparent header with position absolute
    
    * add font folder
    
    * add font-face for each font
    
    * font-family changed
    
    * add background feature to landing
    
    * add image to background component
    
    * add hero-section
    
    * adds gift card section
    
    * l
    
    * 019/about us (#22)
    
    * add vector for the title component
    
    * add image example
    
    * add background for the component
    
    * add about us component
    
    * adds missing files
    
    * adds footer and gift cards (#24)
    
    * add menu section component (#28)
    
    * Add-testimonial-and-catering-code (#25)
    
    * add testimonial component code
    
    * add testimonials further details
    
    * add testimonials further details
    
    * resolved merge conflict on pull request
    
    ---------
    
    Co-authored-by: Henryesc <122941763+Henryesc@users.noreply.github.com>
    
    * >>>>>
    
    * Add testimonial and catering code (#33)
    
    * add testimonial component code
    
    * add testimonials further details
    
    * add testimonials further details
    
    * resolved merge conflict on pull request
    
    * add fine tuning to testimonials
    
    ---------
    
    Co-authored-by: BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
    
    * Merge branch 'develop' of https://github.com/Henryesc/Capstone into develop
    
    * Review01 (#35)
    
    * >>>>>
    
    * adds review component
    
    * delete line 142 because it is empty
    
    * adds vercel.json again
    
    * connects server routes
    
    ---------
    
    Co-authored-by: John Garcia <98444386+jhongarcian@users.noreply.github.com>
    Co-authored-by: BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const server = express();
 const es6Renderer = require("express-es6-template-engine")
+const pgp = require("pg-promise")()
+require("dotenv").config()
+const db = pgp(process.env.URL)
+
+
 
 server.get('/heartbeat', (req, res) => {
     res.json({
