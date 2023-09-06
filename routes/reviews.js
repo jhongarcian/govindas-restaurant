92feaedd602e2211e560f8515b754c3b509bfd03
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-09-06 19:35:39 +0200

    feat (reviews) created a new endpoint that returns 10 latest reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const { postReview } = require("../utils/queries");
+const { postReview, getReviewData } = require("../utils/queries");
 
 
 router.post("/add-review", async (req, res) => {
@@ -12,7 +12,17 @@ router.post("/add-review", async (req, res) => {
     } catch (error) {
         console.error("Error,", error);
         res.status(500).json({message: "Something went wrong"});
-    }
-})
+    };
+});
+
+router.get("/api/reviews", async (req, res) => {
+    try {
+        const review = await getReviewData();
+        res.status(200).json(review);
+    } catch (error) {
+        console.error("Error fetching review:", error);
+        res.status(500).json({erorr: "Something went wrong"});
+    };
+});
 
 module.exports = router
