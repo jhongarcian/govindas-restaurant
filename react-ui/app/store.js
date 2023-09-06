29c174c3be89226d0c748168e15e7861dcf06f9c
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-09-06 19:33:52 +0200

    feat (store) add slice to redux

--- react-ui/app/store.js
+++ react-ui/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todaysMenuReducer from "../features/todays_menuSlice";
+import reviewsReducer from "../features/get_reviewsSlice";
 
 export default configureStore({
     reducer: {
         todays_menu: todaysMenuReducer,
+        reviews: reviewsReducer,
     }
 });
