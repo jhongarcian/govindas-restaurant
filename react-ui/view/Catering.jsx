a700141a91ef09347779a31df9e37014295c7bc1
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-09-18 18:01:09 -0500

    feat (icons) added svg instead of dependency

--- react-ui/view/Catering.jsx
+++ react-ui/view/Catering.jsx
@@ -1,11 +1,13 @@
 import { lazy, Suspense } from "react";
 const CateringForm = lazy(() => import("../components/catering/CateringForm"));
+import { PostMenu } from "../components";
 
 const Catering = () => {
   return (
     <>
       <Suspense fallback={<div>Loading ...</div>}>
         <CateringForm />
+        <PostMenu />
       </Suspense>
     </>
   );
