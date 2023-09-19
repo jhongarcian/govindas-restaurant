697031f49dbb0b936022bd2f2e305f199fbde392
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-09-18 21:33:13 -0500

    twitter post removed

--- react-ui/view/Catering.jsx
+++ react-ui/view/Catering.jsx
@@ -1,13 +1,12 @@
 import { lazy, Suspense } from "react";
 const CateringForm = lazy(() => import("../components/catering/CateringForm"));
-import { PostMenu } from "../components";
+
 
 const Catering = () => {
   return (
     <>
       <Suspense fallback={<div>Loading ...</div>}>
         <CateringForm />
-        <PostMenu />
       </Suspense>
     </>
   );
